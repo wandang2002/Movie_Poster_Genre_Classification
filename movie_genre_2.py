43e03311a50f3874e45eabebce0fe55b4ea5e325
wandang2002 <dangvietminhquan@gmail.com>
2025-03-09 22:33:36 -0400

    Create movie_genre_2.py

---
+++ movie_genre_2.py
@@ -0,0 +1,14 @@
+import time
+import os
+import random 
+import numpy as np 
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.utils.data as data
+import torchvision.transforms as transforms
+from PIL import Image
+
